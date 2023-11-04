89de28ce3d97fb85047a42b75c8d3c0d659744ce
Saylion <126657247+Saylion@users.noreply.github.com>
2023-11-04 11:44:49 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import gradio as gr
 import os
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+import numpy as np
 import urllib.request
 from argparse import ArgumentParser
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+def video_splitting(local_file, progress=gr.Progress()):
+  input_video = local_file
+  output_dir = os.path.join(BASE_DIR, local_file.filename)
+
+  subclip_start_times = [0, 30, 60]
+  subclip_end_times = [30, 60, 90]
+
+  for i, (start_time, end_time) in enumerate(zip(subclip_start_times, supclip_end_times)):
+    output_file = f"{output_dir}subclip_{i}.mp4"
+    ffmpege_extract_supclip(input_video, start_time, end_time, targetname=output_file)
 
 def swap_visibility():
   return gr.update(visible=True), gr.update(visible=False), gr.update(value=''), gr.update(value=None)
@@ -23,13 +36,13 @@ if __name__ == '__main__':
     gr.Label('This is my first Gradio', show_label=False)
     with gr.Accordion('Main Option'):
       with gr.Row():
-        image_input = gr.File(label="Image")
+        #image_input = gr.File(label="Image")
         with gr.Column() as file_upload_col:
-          local_file = gr.File(label='Audio file')
-          input_file = gr.UploadButton('Upload', file_types=['image'], variant='primary')
-          input_file.upload(process_file_upload, inputs=[input_file], outputs=[local_file, image_input])
+          local_file = gr.File(label='Video file')
+          input_file = gr.UploadButton('Upload', file_types=['video'], variant='primary')
+          input_file.upload(process_file_upload, inputs=[input_file], outputs=[local_file])
 
-         # generate_btn = gr.Button("Generate", variant='primary')
+          generate_btn = gr.Button("Generate", variant='primary')
           
           '''
           show_file_upload_button.click(swap_visibility, outputs=[file_upload_col, yt_link_col, song_input, local_file])
